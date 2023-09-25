f5defa68cb35958117fee49246d5baa7933b73f3
MatheusOliveira047 <ludsonmatheus@gmail.com>
2023-09-25 13:08:09 -0300

    Consulta simples com query methods

--- src/main/java/com/mattcode/workshop/Resources/PostResource.java
+++ src/main/java/com/mattcode/workshop/Resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.mattcode.workshop.Resources;
 
+import com.mattcode.workshop.Resources.util.URL;
 import com.mattcode.workshop.domain.Post;
 
 import com.mattcode.workshop.service.PostService;
@@ -27,6 +28,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text",defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 
